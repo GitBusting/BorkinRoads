add4599c8acf7d45fbe226a515b33bbbc99edbb9
Ataberk Olgun <olgunataberk@gmail.com>
2019-01-12 21:53:43 +0300

    Start building maps infrastructure
    We are still trying to learn the maps API, we are able to
    add markers to the map now.

--- app/src/main/java/com/bitirme/gitbusters/borkinroads/MapActivity.java
+++ app/src/main/java/com/bitirme/gitbusters/borkinroads/MapActivity.java
@@ -7,63 +7,88 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class MapActivity extends FragmentActivity
         implements GoogleMap.OnMyLocationButtonClickListener,
         GoogleMap.OnMyLocationClickListener,
         OnMapReadyCallback {
 
-    private GoogleMap mMap;
+  private GoogleMap mMap;
+  private ArrayList<Marker> markers;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_map);
 
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+    // "Reset Button" will remove all markers from
+    // the screen and clear the coordinates list
+    markers = new ArrayList<>();
+    Button rb = findViewById(R.id.resetButton);
+    rb.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        for (int i = 0; i < markers.size(); i++)
+          markers.get(i).remove();
+        markers.clear();
+      }
+    });
+
+    SupportMapFragment mapFragment =
+            (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+    mapFragment.getMapAsync(this);
+  }
+
+  @Override
+  public void onMapReady(GoogleMap map) {
+    mMap = map;
+    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_COARSE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED)
+    {
+      Toast.makeText(this,"Please give location permissions to this App",
+              Toast.LENGTH_LONG).show();
+      return;
     }
+    // Show users their location
+    mMap.setMyLocationEnabled(true);
+    mMap.setOnMyLocationButtonClickListener(this);
+    mMap.setOnMyLocationClickListener(this);
 
-    @Override
-    public void onMapReady(GoogleMap map) {
-        mMap = map;
-        // TODO: Before enabling the My Location layer, you must request
-        // location permission from the user. This sample does not include
-        // a request for location permission.
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mMap.setMyLocationEnabled(true);
-        mMap.setOnMyLocationButtonClickListener(this);
-        mMap.setOnMyLocationClickListener(this);
-    }
+    // Add markers to the map as user clicks on it
+    mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+      @Override
+      public void onMapClick(LatLng latLng) {
+        markers.add(mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title("Marker added by user")));
+      }
+    });
+  }
 
-    @Override
-    public void onMyLocationClick(@NonNull Location location) {
-        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
-    }
+  @Override
+  public void onMyLocationClick(@NonNull Location location) {
+    // Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+  }
 
-    @Override
-    public boolean onMyLocationButtonClick() {
-        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
-        // Return false so that we don't consume the event and the default behavior still occurs
-        // (the camera animates to the user's current position).
-        return false;
-    }
+  @Override
+  public boolean onMyLocationButtonClick() {
+    // Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+    // Return false so that we don't consume the event and the default behavior still occurs
+    // (the camera animates to the user's current position).
+    return false;
+  }
 }
