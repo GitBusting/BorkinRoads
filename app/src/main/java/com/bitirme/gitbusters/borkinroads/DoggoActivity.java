2c12c8cc1093e34f7111d08ef2ef0cfe2cc735d9
Ataberk Olgun <olgunataberk@gmail.com>
2019-01-13 18:27:50 +0300

    Fix TextView assignments

--- app/src/main/java/com/bitirme/gitbusters/borkinroads/DoggoActivity.java
+++ app/src/main/java/com/bitirme/gitbusters/borkinroads/DoggoActivity.java
@@ -8,23 +8,24 @@ import java.util.Date;
 
 public class DoggoActivity extends AppCompatActivity {
 
-
-
     private TextView name, breed, gender, birthdate, age;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doggo);
 
-       Doggo temp =  new Doggo("SysTemp","Terrier",new Date(2000,1,1),new Date(2019,1,13), Doggo.gender.male);
-       //Doggo selected doggo.
+        name      = findViewById(R.id.name);
+        breed     = findViewById(R.id.breed);
+        gender    = findViewById(R.id.gender);
+        birthdate = findViewById(R.id.birthdate);
+        age       = findViewById(R.id.age);
+
+        Doggo temp =  new Doggo("SysTemp","Terrier",new Date(2000,1,1),new Date(2019,1,13), Doggo.gender.male);
+        //Doggo selected doggo.
 
         name.setText(temp.getName());
         breed.setText(temp.getBreed());
         birthdate.setText(temp.getBirth_date().toString());
         age.setText(temp.getBirth_date().toString()); //broken on purpose
-
-
-
     }
 }
