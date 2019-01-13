a250a1eb7352d4e2062ebdb838f95505653320b2
utkuumur <uacikalin@etu.edu.tr>
2019-01-13 20:11:26 +0300

    Created DisplayRoutesActivity and add dependencies for RecyclerView

---
+++ app/src/main/java/com/bitirme/gitbusters/borkinroads/DisplayRoutesActivity.java
@@ -0,0 +1,13 @@
+package com.bitirme.gitbusters.borkinroads;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DisplayRoutesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_routes);
+    }
+}
