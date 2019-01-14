478efaa5ef1eb25c6aaf8dfb74fb0cabc78f1ee3
Nisa Bostancı <nisabstnc@gmail.com>
2019-01-14 16:22:32 +0300

    Generate HTTP request for generating the route

--- app/src/main/java/com/bitirme/gitbusters/borkinroads/MapActivity.java
+++ app/src/main/java/com/bitirme/gitbusters/borkinroads/MapActivity.java
@@ -3,10 +3,10 @@ package com.bitirme.gitbusters.borkinroads;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,7 +18,12 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MapActivity extends FragmentActivity
         implements GoogleMap.OnMyLocationButtonClickListener,
@@ -46,9 +51,48 @@ public class MapActivity extends FragmentActivity
       }
     });
 
+    Button genPathButton = findViewById(R.id.generatePathButton);
+    genPathButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        getPath(markers,markers.get(0));
+      }
+    });
+
     SupportMapFragment mapFragment =
             (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
     mapFragment.getMapAsync(this);
+  }
+
+  public void getPath(ArrayList<Marker> markers,Marker start){
+    Toast.makeText(this, "getPath clicked.", Toast.LENGTH_SHORT).show();
+    HttpsURLConnection conn = null;
+    try {
+        String parameters;
+        parameters = "origin=" + start.getPosition().latitude + "," + start.getPosition().longitude;
+        parameters += "\n&destination=" + start.getPosition().latitude + "," + start.getPosition().longitude; //start and end points are the same for the route
+        parameters += "\n&key=AIzaSyA3nOUd0mIm1mCoUIx1DRa-qsCT3Kz1a_k"; // this should change i guess
+        parameters += "\n&mode=walking";
+        if(markers.size()>0){
+            parameters += "\n&waypoints=";
+        }
+        for(int i =0;i<markers.size();i++) {
+            if(i>=10) break;
+            if(i!=0) parameters += "|";
+            parameters += "via:" + markers.get(i).getPosition().latitude + "," + markers.get(i).getPosition().longitude;
+        }
+        URL webServerUrl = new URL("https://maps.googleapis.com/maps/api/directions/json?"+parameters);
+        conn =(HttpsURLConnection) webServerUrl.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+
+        conn.setRequestMethod("GET");
+        conn.connect();
+    }
+    catch (IOException e) {
+        e.printStackTrace();
+    }
+
   }
 
   @Override
@@ -81,7 +125,7 @@ public class MapActivity extends FragmentActivity
 
   @Override
   public void onMyLocationClick(@NonNull Location location) {
-    // Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+     Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
   }
 
   @Override
