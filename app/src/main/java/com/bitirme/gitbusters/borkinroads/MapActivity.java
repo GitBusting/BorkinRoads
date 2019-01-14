811e81d94ffd9a9ba4372b633717db6d0b4a915c
Nisa Bostancı <nisabstnc@gmail.com>
2019-01-14 17:41:28 +0300

    Send HTTP request for generating the route

--- app/src/main/java/com/bitirme/gitbusters/borkinroads/MapActivity.java
+++ app/src/main/java/com/bitirme/gitbusters/borkinroads/MapActivity.java
@@ -3,10 +3,10 @@ package com.bitirme.gitbusters.borkinroads;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -25,70 +25,84 @@ public class MapActivity extends FragmentActivity
         GoogleMap.OnMyLocationClickListener,
         OnMapReadyCallback {
 
-  private GoogleMap mMap;
-  private ArrayList<Marker> markers;
+    private GoogleMap mMap;
+    private ArrayList<Marker> markers;
+    private ArrayList<LatLng> coordinates;
+    private static final DirectionsHandler requester = new DirectionsHandler();
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_map);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
 
-    // "Reset Button" will remove all markers from
-    // the screen and clear the coordinates list
-    markers = new ArrayList<>();
-    Button rb = findViewById(R.id.resetButton);
-    rb.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        for (int i = 0; i < markers.size(); i++)
-          markers.get(i).remove();
-        markers.clear();
-      }
-    });
+        // "Reset Button" will remove all markers from
+        // the screen and clear the coordinates list
+        markers = new ArrayList<>();
+        coordinates = new ArrayList<>();
+        Button rb = findViewById(R.id.resetButton);
+        rb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for (int i = 0; i < markers.size(); i++)
+                    markers.get(i).remove();
+                markers.clear();
+                coordinates.clear();
+            }
+        });
 
-    SupportMapFragment mapFragment =
-            (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-    mapFragment.getMapAsync(this);
-  }
+        Button genPathButton = findViewById(R.id.generatePathButton);
+        genPathButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requester.setPath(coordinates,coordinates.get(0));
+                requester.start();
+            }
+        });
 
-  @Override
-  public void onMapReady(GoogleMap map) {
-    mMap = map;
-    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
-            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
-            Manifest.permission.ACCESS_COARSE_LOCATION) !=
-            PackageManager.PERMISSION_GRANTED)
-    {
-      Toast.makeText(this,"Please give location permissions to this App",
-              Toast.LENGTH_LONG).show();
-      return;
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
-    // Show users their location
-    mMap.setMyLocationEnabled(true);
-    mMap.setOnMyLocationButtonClickListener(this);
-    mMap.setOnMyLocationClickListener(this);
 
-    // Add markers to the map as user clicks on it
-    mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-      @Override
-      public void onMapClick(LatLng latLng) {
-        markers.add(mMap.addMarker(new MarkerOptions()
-                .position(latLng)
-                .title("Marker added by user")));
-      }
-    });
-  }
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mMap = map;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED)
+        {
+            Toast.makeText(this,"Please give location permissions to this App",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        // Show users their location
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
 
-  @Override
-  public void onMyLocationClick(@NonNull Location location) {
-    // Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
-  }
+        // Add markers to the map as user clicks on it
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                markers.add(mMap.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .title("Marker added by user")));
+                coordinates.add(latLng);
+            }
+        });
+    }
 
-  @Override
-  public boolean onMyLocationButtonClick() {
-    // Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
-    // Return false so that we don't consume the event and the default behavior still occurs
-    // (the camera animates to the user's current position).
-    return false;
-  }
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        // Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
 }
