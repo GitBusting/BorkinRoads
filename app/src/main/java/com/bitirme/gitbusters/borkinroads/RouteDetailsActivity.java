59881abddf391e7d4b7413cb398ce930ac3aae2b
utkuumur <uacikalin@etu.edu.tr>
2019-02-03 22:32:23 +0300

    Basic details layout

---
+++ app/src/main/java/com/bitirme/gitbusters/borkinroads/RouteDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.bitirme.gitbusters.borkinroads;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RouteDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_details);
+
+    }
+}
