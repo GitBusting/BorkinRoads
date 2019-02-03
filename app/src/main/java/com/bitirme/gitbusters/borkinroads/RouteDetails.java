28b9f0fa5a638e15bf74c4f5b2d440ec589ac08d
utkuumur <uacikalin@etu.edu.tr>
2019-02-03 22:50:01 +0300

    Class for storing route details

---
+++ app/src/main/java/com/bitirme/gitbusters/borkinroads/RouteDetails.java
@@ -0,0 +1,48 @@
+package com.bitirme.gitbusters.borkinroads;
+
+public class RouteDetails {
+    private double maxPace;
+    private double avgPace;
+    private double maxSpeed;
+    private double avgSpeed;
+    private double routeLength;
+    private double totalTime;
+    private double movingTime;
+    private String data, time;
+
+    public double getMaxPace() {
+        return maxPace;
+    }
+
+    public double getAvgPace() {
+        return avgPace;
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public double getAvgSpeed() {
+        return avgSpeed;
+    }
+
+    public double getRouteLength() {
+        return routeLength;
+    }
+
+    public double getTotalTime() {
+        return totalTime;
+    }
+
+    public double getMovingTime() {
+        return movingTime;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
