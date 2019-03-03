da229cb71d0355e1ea921becb20b3e5bcc5ee1c5
Bahattin Maral <bahattinmaral@gmail.com>
2019-03-03 21:28:22 +0300

    Added pet synchronization with the server database. #16

--- app/src/main/java/com/bitirme/gitbusters/borkinroads/Doggo.java
+++ app/src/main/java/com/bitirme/gitbusters/borkinroads/Doggo.java
@@ -1,10 +1,16 @@
 package com.bitirme.gitbusters.borkinroads;
 
+import com.bitirme.gitbusters.borkinroads.data.RestRecordImpl;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
-public class Doggo {
+public class Doggo extends RestRecordImpl {
     static final ArrayList<Doggo> doggos = new ArrayList<>();
     private String name;
     private String breed;
@@ -12,7 +18,17 @@ public class Doggo {
     private ZonedDateTime last_walk_date;
     private ZonedDateTime last_bath_date;
     private ZonedDateTime last_vet_date;
+    private int entryID;
 
+
+    public Doggo() {
+        super();
+    }
+
+    public Doggo(JSONObject jso) {
+        super(jso);
+        parseRecordFromJSON(jso);
+    }
 
     Doggo(String name, String breed, ZonedDateTime birth_date, gender sex) {
         this.name = name;
@@ -84,5 +100,65 @@ public class Doggo {
         this.last_vet_date = last_vet_date;
     }
 
+    private void parseRecordFromJSON(JSONObject jso) {
+        try {
+            this.entryID = jso.getInt("id");
+            this.name = jso.getString("name");
+            this.breed = jso.getString("breed");
+            String jsoGender = jso.getString("gender");
+            if (jsoGender.equals("female"))
+                this.sex = gender.Female;
+            else
+                this.sex = gender.Male;
+            this.birth_date = parseSTRtoZTD(jso.getString("birthDate"));
+            this.last_walk_date = parseSTRtoZTD(jso.getString("lastWalkDate"));
+            this.last_bath_date = parseSTRtoZTD(jso.getString("lastBathDate"));
+            this.last_vet_date = parseSTRtoZTD(jso.getString("lastVetDate"));
+        } catch (JSONException jse) {
+            jse.printStackTrace();
+        }
+    }
+
+    private ZonedDateTime parseSTRtoZTD(String date) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
+        return ZonedDateTime.parse(date, formatter);
+    }
+
+    private String parseZTDtoSTR(ZonedDateTime birthDate) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
+        return birthDate.format(formatter);
+    }
+
+    @Override
+    public String getURL() {
+        return "https://shielded-cliffs-47552.herokuapp.com/doggos";
+    }
+
+    @Override
+    public JSONObject getJSON() {
+        JSONObject jso = new JSONObject();
+        try {
+            jso.put("name", this.name);
+            jso.put("breed", this.breed);
+            if (this.sex == gender.Female)
+                jso.put("gender", "female");
+            else
+                jso.put("gender", "male");
+            jso.put("birthDate", parseZTDtoSTR(this.birth_date));
+            jso.put("lastWalkDate", parseZTDtoSTR(this.last_walk_date));
+            jso.put("lastBathDate", parseZTDtoSTR(this.last_bath_date));
+            jso.put("lastVetDate", parseZTDtoSTR(this.last_vet_date));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jso;
+    }
+
+    @Override
+    public int getEntryID() {
+        return entryID;
+    }
 
 }
