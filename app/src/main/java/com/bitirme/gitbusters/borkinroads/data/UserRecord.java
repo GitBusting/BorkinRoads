a7aa33240ae0f98c013ec42f8dfbd7a2e5800d2d
Ataberk <olgunataberk@gmail.com>
2019-03-10 23:32:57 +0300

    Implement friend list activity
    This template activity displays user's friends as they are fetched
    from the database. The activity is expected to enable searching through
    users in database.
    
    # Conflicts:
    #	app/src/main/java/com/bitirme/gitbusters/borkinroads/MapActivity.java

--- app/src/main/java/com/bitirme/gitbusters/borkinroads/data/UserRecord.java
+++ app/src/main/java/com/bitirme/gitbusters/borkinroads/data/UserRecord.java
@@ -10,21 +10,28 @@ public class UserRecord extends RestRecordImpl {
   private String URL = "Still Empty";
 
   private int userID;
+  private String name;
   private ArrayList<Integer> petIDs;
 
-  public UserRecord(int userID, ArrayList<Integer> petIDs) {
+  public UserRecord(){};
+
+  public UserRecord(String name, int userID, ArrayList<Integer> petIDs) {
+    this.name   = name;
     this.userID = userID;
     this.petIDs = petIDs;
   }
 
   public UserRecord(JSONObject jso) {
     if(URL.equals("Still Empty"))
-      throw new AssertionError("PublicRouteRecord class does not" +
+      throw new AssertionError("UserRecord class does not" +
               "have a valid URL yet!");
 
     try {
+      this.name      = jso.getString("name");
       this.userID    = jso.getInt("userID");
-
+      this.petIDs    = new ArrayList<>();
+      for(String pet : jso.getString("petID").split(","))
+        this.petIDs.add(Integer.parseInt(pet));
     } catch (JSONException e) {
       e.printStackTrace();
     }
@@ -44,4 +51,6 @@ public class UserRecord extends RestRecordImpl {
   public int getEntryID() {
     return 0;
   }
+
+  public String getName() { return this.name; }
 }
--- app/src/main/java/com/bitirme/gitbusters/borkinroads/routeutilities/FriendRouteHandler.java
+++ app/src/main/java/com/bitirme/gitbusters/borkinroads/routeutilities/FriendRouteHandler.java
@@ -7,13 +7,15 @@ import com.akexorcist.googledirection.GoogleDirection;
 import com.akexorcist.googledirection.constant.TransportMode;
 import com.akexorcist.googledirection.model.Direction;
 import com.bitirme.gitbusters.borkinroads.MapActivity;
-import com.bitirme.gitbusters.borkinroads.data.PublicRouteRecord;
 import com.bitirme.gitbusters.borkinroads.data.RestRecordImpl;
+import com.bitirme.gitbusters.borkinroads.data.UserStatusRecord;
 import com.bitirme.gitbusters.borkinroads.dbinterface.RestPuller;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Responsible for displaying and updating routes
@@ -22,10 +24,15 @@ import java.util.Collections;
  */
 public class FriendRouteHandler extends Thread implements DirectionCallback {
 
+  private static final boolean SANDBOX = true;
+  private static final LatLng SBOX_START = new LatLng(39.9185,32.8562983);
+  private static final LatLng SBOX_END = new LatLng(39.9185,32.8562983);
+  private static final ArrayList<LatLng> SBOX_WAYPS = new ArrayList<>();
+
   /* To be assigned when MapActivity is created */
   private static MapActivity mapReference;
 
-  private ArrayList<PublicRouteRecord> activeRoutes;
+  private ArrayList<UserStatusRecord> activeRoutes;
 
   private CountDownTimer cdt;
 
@@ -33,6 +40,14 @@ public class FriendRouteHandler extends Thread implements DirectionCallback {
   {
     super();
     activeRoutes = new ArrayList<>();
+    if(SANDBOX)
+    {
+      SBOX_WAYPS.add(new LatLng(39.91893273133,32.860237509));
+      SBOX_WAYPS.add(new LatLng(39.92218008905,32.8554963693));
+      SBOX_WAYPS.add(new LatLng(39.9202369,32.8495951741));
+      SBOX_WAYPS.add(new LatLng(39.9165384488,32.852128520));
+    }
+
   }
 
   public static void setMapReference(MapActivity mapref)
@@ -43,48 +58,62 @@ public class FriendRouteHandler extends Thread implements DirectionCallback {
   @Override
   public void run()
   {
+    if(SANDBOX)
+      Logger.getGlobal().log(Level.WARNING, "FriendRouteHandler is working in SANDBOX mode!");
+    frhLoop();
+  }
+
+  private final void frhLoop()
+  {
+    RestPuller rp = new RestPuller(new UserStatusRecord());
+    if(!SANDBOX)
+      rp.start();
+    // Get new records from the database
+    ArrayList<UserStatusRecord> newRecords = new ArrayList<>();
+    if(!SANDBOX)
+    {
+      for (RestRecordImpl rri : rp.getFetchedRecords())
+        newRecords.add((UserStatusRecord) rri);
+      Collections.sort(newRecords);
+    }else
+    {
+      newRecords.add(new UserStatusRecord(-1, -1, true,
+          SBOX_START, SBOX_WAYPS, SBOX_START, SBOX_END));
+    }
+    // Compare newly fetched routes with already existing ones
+    // if there exists a new route we tell the MapActivity to display it
+    if(activeRoutes.size() != newRecords.size())
+    {
+      for (UserStatusRecord usr : newRecords)
+      {
+        boolean exists = false;
+        for (UserStatusRecord act : activeRoutes)
+        {
+          if(act.equals(usr))
+          {
+            exists = true;
+            break;
+          }
+        }
+        if(!exists)
+        {
+          ArrayList<LatLng> ll = usr.getWaypoints();
+          requestDirection(usr.getStartPoint(), usr.getEndPoint(), ll);
+          activeRoutes.add(usr);
+        }
+      }
+    }
+
+    // TODO update friend indicators on map
+    for(UserStatusRecord usr : activeRoutes)
+      mapReference.putFriendMarker(new FriendMarker(usr.getCurrentPosition()));
+
     cdt = new CountDownTimer(20000, 10000) {
       public void onTick(long millisUntilFinished) {
         System.out.println("FriendRouteHandler: timer heartbeat");
       }
       public void onFinish() {
-        RestPuller rp = new RestPuller(new PublicRouteRecord());
-        rp.start();
-        // Get new records from the database
-        ArrayList<PublicRouteRecord> newRecords = new ArrayList<>();
-        for(RestRecordImpl rri : rp.getFetchedRecords())
-          newRecords.add((PublicRouteRecord) rri);
-        Collections.sort(newRecords);
-
-        // Compare newly fetched routes with already existing ones
-        // if there exists a new route we tell the MapActivity to display it
-        if(activeRoutes.size() != newRecords.size())
-        {
-          for (PublicRouteRecord prr : newRecords)
-          {
-            boolean exists = false;
-            for (PublicRouteRecord act : activeRoutes)
-            {
-              if(act.equals(prr))
-              {
-                exists = true;
-                break;
-              }
-            }
-            if(!exists)
-            {
-              ArrayList<LatLng> ll = prr.getWaypoints();
-              requestDirection(ll.remove(0), ll.remove(ll.size()-1),ll);
-              activeRoutes.add(prr);
-            }
-          }
-        }
-
-        // TODO update friend indicators on map
-        ArrayList<FriendMarker> fms = new ArrayList<>();
-        for(PublicRouteRecord prr : activeRoutes)
-          fms.add(new FriendMarker(prr.getLocation()));
-        mapReference.displayFriendMarkers(fms);
+        frhLoop();
       }
     }.start();
   }
@@ -100,7 +129,7 @@ public class FriendRouteHandler extends Thread implements DirectionCallback {
 
   @Override
   public void onDirectionSuccess(Direction direction, String rawBody) {
-      mapReference.displayFriendRoute(direction);
+      mapReference.putFriendDirection(direction);
   }
 
   @Override
